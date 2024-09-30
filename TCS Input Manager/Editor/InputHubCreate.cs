480999db15325c65856438ae8096161fe66c39e3
Damon Fedorick <95268795+Ddemon26@users.noreply.github.com>
2024-09-30 01:22:55 -0700

    removed a font

--- TCS Input Manager/Editor/InputHubCreate.cs
+++ TCS Input Manager/Editor/InputHubCreate.cs
@@ -1,21 +1,20 @@
 using UnityEditor;
 using UnityEngine;
+
 namespace TCS.InputSystem.Editor {
     public static class InputHubCreate {
         const string INPUT_HUB = "<color=#00FF00ff>[InputHub]</color>";
 
-        [MenuItem("Tools/TCS/Create InputHub")]
-        static void CreateInputHub() {
-            // Check if an InputHub already exists in the scene
-            if (!Object.FindFirstObjectByType<InputHub>(FindObjectsInactive.Include)) {
-                // Create a new GameObject named InputHub
-                var inputHub = new GameObject("InputHub");
-                // Add the InputHub component to the GameObject
-                inputHub.AddComponent<InputHub>();
-            }
-            else {
+        [MenuItem("Tools/TCS/Managers/Spawn InputHub")]
+        public static void CreateInputHub() {
+            if (Object.FindAnyObjectByType<InputHub>(FindObjectsInactive.Include)) {
                 Debug.LogWarning($"{INPUT_HUB} An InputHub already exists in the scene.");
+                return;
             }
+
+            var inputHub = new GameObject("[InputHub]");
+            inputHub.AddComponent<InputHub>();
+            Debug.Log($"{INPUT_HUB} InputHub spawned successfully.");
         }
     }
 }
